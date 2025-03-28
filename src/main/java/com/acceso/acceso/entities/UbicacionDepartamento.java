57dd177bac2627a0336f723ef3245ee0ff75e89a
Mirko Gutierrez <mirko.gutierrez.dev@gmail.com>
2025-03-28 16:12:57 -0300

    agrega tabla de ubicaciones

---
+++ src/main/java/com/acceso/acceso/entities/UbicacionDepartamento.java
@@ -0,0 +1,105 @@
+package com.acceso.acceso.entities;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "ubicaciones_departamentos")
+public class UbicacionDepartamento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String direccion;
+
+    private String numero;
+
+    private String piso;
+
+    private String referencia;
+
+    private Double latitud;
+
+    private Double longitud;
+
+    @OneToMany(mappedBy = "ubicacion", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<Departamento> departamentos = new HashSet<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getPiso() {
+        return piso;
+    }
+
+    public void setPiso(String piso) {
+        this.piso = piso;
+    }
+
+    public String getReferencia() {
+        return referencia;
+    }
+
+    public void setReferencia(String referencia) {
+        this.referencia = referencia;
+    }
+
+    public Double getLatitud() {
+        return latitud;
+    }
+
+    public void setLatitud(Double latitud) {
+        this.latitud = latitud;
+    }
+
+    public Double getLongitud() {
+        return longitud;
+    }
+
+    public void setLongitud(Double longitud) {
+        this.longitud = longitud;
+    }
+
+    public Set<Departamento> getDepartamentos() {
+        return departamentos;
+    }
+
+    public void setDepartamentos(Set<Departamento> departamentos) {
+        this.departamentos = departamentos;
+    }
+
+ 
+}
+
